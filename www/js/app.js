16b442b522b2e34c3f5e25e98e3ea3c0a538822e
William Collier <penguinwithwaffle@gmail.com>
2015-02-01 15:59:08 +0000

    Screw Google maps

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'ngMessages', 'ionic.rating', 'elif'])
+angular.module('starter', ['ionic', 'starter.controllers', 'ngMessages', 'ionic.rating', 'elif', 'uiGmapgoogle-maps'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -30,6 +30,15 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ngMessages', 'ionic.
     controller: 'AppCtrl'
   })
 
+  .state('app.map', {
+    url: "/locations",
+    views: {
+      'menuContent': {
+          templateUrl: "templates/locations.html"
+       }
+    }
+  })
+  
   .state('app.attractions', {
     url: "/attractions",
 	views: {
@@ -63,8 +72,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ngMessages', 'ionic.
         url: "/location",
         views: {
             'menuContent@app': {
-                templateUrl: "templates/places/place/location.html",
-                controller: 'MapCtrl'
+                templateUrl: "templates/places/place/location.html"
             }
         }
     })
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -25,8 +25,8 @@ angular.module('starter.controllers', [])
     
     // This object needs revision
     $rootScope.placeDescription = [
-        { name: 'Place 1', attractions: [$rootScope.attractionDescription[0], $rootScope.attractionDescription[1]], reviews: [$rootScope.placeReviews[0], $rootScope.placeReviews[1]], description: 'Place 1 descriptionPlace 1 descriptionPlace 1 descriptionPlace 1 description', position: {x: "51.500729", y: "-0.124625"}, id: 0},
-        { name: 'Place 2', attractions: [$rootScope.attractionDescription[1]], reviews: [$rootScope.placeReviews[1]], description: 'Place 2 description', position: {x: "-123", y: "4234"}, id: 1 },
+        { name: 'Place 1', attractions: [$rootScope.attractionDescription[0], $rootScope.attractionDescription[1]], reviews: [$rootScope.placeReviews[0], $rootScope.placeReviews[1]], description: 'Place 1 descriptionPlace 1 descriptionPlace 1 descriptionPlace 1 description', latitude: 51.500729, longitude: -0.124625, options: { contentString: "Hello"}, id: 0},
+        { name: 'Place 2', attractions: [$rootScope.attractionDescription[1]], reviews: [$rootScope.placeReviews[1]], description: 'Place 2 description', latitude: 51.499479, longitude: -0.124809, id: 1 },
     ];
     
 	// They are hardcoded but it does not matter
@@ -34,6 +34,7 @@ angular.module('starter.controllers', [])
 		$rootScope.placeDescription[0],
         $rootScope.placeDescription[1]
 	];
+
 })
 .controller('AppCtrl', function() {
 })
@@ -44,76 +45,12 @@ angular.module('starter.controllers', [])
 
 })
 
-.controller('PlaceCtrl', function($scope, $stateParams) {
-	// Screw it, don't know how to access '$rootScope' in the template just assign a variable insted. Just a mirage (calling the two the same name)
+.controller('PlaceCtrl', function($scope, $stateParams, $rootScope) {
+    
 	$scope.id = $stateParams.placeId;
 
-	// This is the maximum star rating which a review can have
-	$scope.max = 5;
-  	/*
-     * if given dropdown is the selected dropdown, deselect it
-     * else, select the given dropdown
-     */
-    $scope.toggleDropdown = function(dropdown) {
-        if ($scope.isDropdownShown(dropdown)) {
-            $scope.showDropdown = null;
-        } else {
-            $scope.showDropdown = dropdown;
-        }
-    };
-    
-    $scope.isDropdownShown = function(dropdown) {
-        return $scope.showDropdown === dropdown;
-    };	
-    
 })
 
 .controller('AttractionCtrl', function($scope, $stateParams) {
     $scope.attractionId = $stateParams.attractionId;
-})
-
-.controller('MapCtrl', function($scope, $ionicLoading, $compile) {
-    
-    $scope.init = function(place) {
-        var myLatlng = new google.maps.LatLng(place.position.x, place.position.y);
-
-        var mapOptions = {
-            center: myLatlng,
-            zoom: 16,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        
-        var map = new google.maps.Map(document.getElementById("map"),
-                                      mapOptions);
-
-        //Marker + infowindow + angularjs compiled ng-click
-    //    var contentString = "<div id='bodyContent'>" + place.description + "</div>";
-      
-        var contentString = '<div id="content">'+
-                                  '<div id="siteNotice">'+
-                                  '</div>'+
-                                  '<h1 id="firstHeading" class="firstHeading">' + place.name  + '</h1>'+
-                                  '<div id="bodyContent">'+
-                                          place.description + 
-                                   '</div>'+
-                              '</div>';
-        
-        var compiled = $compile(contentString)($scope);
-
-        var infowindow = new google.maps.InfoWindow({ content: compiled[0] });
-
-        var marker = new google.maps.Marker({
-            position: myLatlng,
-            map: map,
-            title: place.name
-        });
-
-        google.maps.event.addListener(marker, 'click', function() {
-            infowindow.setContent(contentString);
-            infowindow.open(map,marker);
-        });
-
-        $scope.map = map;
-    };
-
 });
